5beef9ae8645c2b2d2fd5d3d637ce13e2b4b1616
vishals <vishals@PRINHYLTPAP1333.local>
2019-02-07 17:17:43 +0530

    refactored

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,58 +1,82 @@
 import React, { Component } from 'react';
-import './Dashboard.css';
+import './Dashboard.scss';
 import Post from './Post/Post';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../store/actions';
 
 class Dashboard extends Component {
 
-  state = {
-    subreddits: ['pics', 'cats']
-  }
+    state = {
+        subreddits: [
+            'Alternativeart',
+            'Pics',
+            'Gifs',
+            'Adviceanimals',
+            'Cats',
+            'Images',
+            'Photoshopbattles',
+            'Hmmm',
+            'All',
+            'Aww'
+        ]
+    }
 
-  componentDidMount() {
-    this.props.putPostDataToStore('pics');
-  }
+    componentDidMount() {
+        this.props.putPostDataToStore('pics');
+    }
 
 
-  createPostsList() {
-    return this.props.posts.map(post => {
-      return (
-        <Post key={post.id} data={post}></Post>
-      );
-    });
-  }
+    createPostsList() {
+        return this.props.posts.map(post => {
+            return (
+                <Post key={post.id} data={post}></Post>
+            );
+        });
+    }
 
-  createHeader() {
-    return this.state.subreddits.map(subreddit => {
+    createHeader() {
+        return this.state.subreddits.map(subreddit => {
+            return (
+                <li className="nav-item">
+                    <a className="nav-link Cursor" onClick={() => this.props.putPostDataToStore(subreddit.toLowerCase())}>{subreddit}</a>
+                </li>
+            );
+        });
+    }
+
+    render() {
         return (
-            <button onClick={() => this.props.putPostDataToStore(subreddit)}>{subreddit}</button>
+            <div className="Dashboard">
+                {<nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <a className="navbar-brand" href="#">SubReddits</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            {
+                                this.createHeader()
+                            } 
+                        </ul>
+                    </div>
+                </nav>
+                }
+                {
+                    this.createPostsList()
+                }
+            </div>
         );
-    });
-  }
-
-  render() {
-    return (
-      <div className="Dashboard">
-        { 
-          this.createHeader()
-        }
-        {
-          this.createPostsList()
-        }
-      </div>
-    );
-  }
+    }
 }
 const mapStateToProps = state => {
-  return {
-    posts: state.posts
-  };
+    return {
+        posts: state.posts
+    };
 };
 
 const mapDispatchToProps = dispatch => {
-  return {
-    putPostDataToStore: (subreddit) => dispatch(fetchPosts(subreddit))
-  };
+    return {
+        putPostDataToStore: (subreddit) => dispatch(fetchPosts(subreddit))
+    };
 };
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
--- src/Dashboard/Post/Post.js
+++ src/Dashboard/Post/Post.js
@@ -3,9 +3,18 @@ import './Post.css';
 
 const post = (props) => {
     return (
-        <div className="Post">
-            <h1>{props.data.data.title}</h1>
-            <img className="Image" src={props.data.data.url}></img>
+        <div className="card border-dark mb-3 Card Post">
+            <div className="card-header bg-transparent border-dark">{props.data.data.title}</div>
+            <div className="card-body text-success">
+                <img className="Image" src={props.data.data.url}></img>
+            </div>
+            <div className="card-footer bg-transparent border-dark">
+                <p>Ups: {props.data.data.ups}</p>
+                <p>Downs: {props.data.data.downs}</p>
+            </div>
+            <div className="card-footer Author">
+                <small className="text-muted">{props.data.data.author_fullname}</small>
+            </div>
         </div>
     )
 };
